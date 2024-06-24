452decd3104d40453604dea76e7d9cf9b92d5698
bobbybwoy <68812513+bobbybwoy@users.noreply.github.com>
2024-06-24 16:19:46 +0100

    Expanded the calculator to include more operators

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -1,6 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Welcome to the calculator");
+Console.WriteLine("=========================");
+
+Console.Write("Please enter the operator: ");
+string arithmeticOperator = Console.ReadLine();
 
 Console.Write("Enter the first number: ");
 int firstNumber = int.Parse(Console.ReadLine());
@@ -8,7 +12,37 @@ int firstNumber = int.Parse(Console.ReadLine());
 Console.Write("Enter the second number: ");
 int secondNumber = int.Parse(Console.ReadLine());
 
-int result = firstNumber * secondNumber;
+int result = 0;
 
-Console.WriteLine("Result: " + result);
+// if (arithmeticOperator == "+")
+//     result = firstNumber + secondNumber;
+// else if (arithmeticOperator == "-")
+//     result = firstNumber - secondNumber;
+// else if (arithmeticOperator == "*")
+//     result = firstNumber * secondNumber;
+// else if (arithmeticOperator == "/")
+//     result = firstNumber / secondNumber;
+// else
+//     Console.WriteLine("Operator {0} is invalid.", arithmeticOperator);
+
+switch (arithmeticOperator)
+{
+    case "+":
+        result = firstNumber + secondNumber;
+        break;
+    case "-":
+        result = firstNumber - secondNumber;
+        break;
+    case "*":
+        result = firstNumber * secondNumber;
+        break;
+    case "/":
+        result = firstNumber / secondNumber;
+        break;
+    default:
+        Console.WriteLine("Operator {0} is invalid.", arithmeticOperator);
+        break;
+}
+
+Console.WriteLine("The answer is: " + result);
 Console.ReadLine();
