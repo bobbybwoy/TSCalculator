6c7d7e714716f95a3b1a71967b9294129d239a1a
bobbybwoy <68812513+bobbybwoy@users.noreply.github.com>
2024-06-24 14:09:42 +0100

    Added a simple multiplier

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -1,4 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Welcome to the calculator");
+
+Console.Write("Enter the first number: ");
+int firstNumber = int.Parse(Console.ReadLine());
+
+Console.Write("Enter the second number: ");
+int secondNumber = int.Parse(Console.ReadLine());
+
+int result = firstNumber * secondNumber;
+
+Console.WriteLine("Result: " + result);
 Console.ReadLine();
